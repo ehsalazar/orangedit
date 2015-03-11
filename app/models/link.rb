e88727118f10bfea5b30df6a65748563ac966499
Ernie Salazar <ehsalazar75@gmail.com>
2015-03-11 15:56:52 -0700

    Update links model to generate the title after urls are submitted.

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,13 @@ class Link < ActiveRecord::Base
   belongs_to :user
   acts_as_votable
   has_many :comments
+  after_create :set_title
+
+  def set_title
+    url = self.url
+    title = Mechanize.new.get(url).title
+    self.title = title
+    self.save
+  end
+
 end
